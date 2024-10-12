3e11d538e8929b3b283c3cf7ae5b447241eb532f
1Pisar <b.pisarz124@gmail.com>
2024-10-12 15:04:26 +0200

    Zmiana

--- Main.java
+++ Main.java
@@ -1,5 +1,5 @@
-public class Main { no usages new*
-    public static void main(String[] args) { no usages new*
-        System.out.println("Hello World");
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World_");
     }
 }
